7ee726f562c0faa5a942a78d6dd65482f97dff58
P Sathya Narayan <sathyanmbpk@gmail.com>
2023-04-14 08:24:18 +0530

    Added get request

--- main.py
+++ main.py
@@ -8,27 +8,31 @@ with open('modelKNN.pkl', 'rb') as f:
 X_train = np.load('X_train.npy',allow_pickle=True)
 y_train = np.load('y_train.npy',allow_pickle=True)
 
+
 # Create a Flask app
 app = Flask(__name__)
-
 # Define an endpoint for making predictions
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET','POST'])
 def predict():
     # Get the input data from the user
-    input_data = request.get_json()
+    global prediction
+    if request.method == 'POST':
+        input_data = request.get_json()
 
-    # Convert the input data to a numpy array
-    input_array = np.array(list(input_data.values())).reshape(1, -1)
+        # Convert the input data to a numpy array
+        input_array = np.array(list(input_data.values())).reshape(1, -1)
 
-    # Make a prediction using the trained model
-    prediction = model.predict(input_array)[0]
-    print(prediction)
-    # Convert prediction to a float
-    # val = y_train[prediction-1]
-    # prediction = (prediction)
-    # print(prediction)
-    # Return the prediction to the user in JSON format
-    return jsonify({'prediction': prediction})
+        # Make a prediction using the trained model
+        prediction = model.predict(input_array)[0]
+        print(prediction)
+        # Convert prediction to a float
+        # val = y_train[prediction-1]
+        # prediction = (prediction)
+        # print(prediction)
+        # Return the prediction to the user in JSON format
+        return jsonify({'prediction': prediction})
+    if request.method == 'GET':
+        return prediction
 
 
 
